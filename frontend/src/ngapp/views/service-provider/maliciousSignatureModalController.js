7c04b29a3cb9da42f4e384ab75ff9172c60652b8
Anuj Gupta <azg0076@auburn.edu>
2018-06-14 12:09:26 -0500

    Added flexibility on whether to re-calculate Voronoi diagram on service provider tab.

--- frontend/src/ngapp/views/service-provider/maliciousSignatureModalController.js
+++ frontend/src/ngapp/views/service-provider/maliciousSignatureModalController.js
@@ -2,17 +2,19 @@ import ng from 'angular';
 import ngapp from '../../ngappmodule.js';
 import crypto from '../../services/crypto.js';
 import dataService from '../../services/data.js';
-
+import voronoiDiagram from '../../../voronoi-util/voronoi.js';
+import util from '../../services/util.js';
 
 const CONTROLLER_NAME = 'maliciousSignatureModalController';
 
 var def = {
-	'controller': ['$scope', crypto.toString(), dataService.toString(), function ($scope, crypto, dataService) {
+	'controller': ['$scope', crypto.toString(), dataService.toString(), util.toString(), function ($scope, crypto, dataService, util) {
 		var $ctrl = this;
 
 		var keypair = {};
 		$ctrl.crypto = crypto;
 		$scope.keypair = keypair;
+		$scope.diff = util.diffArrays($scope.POIsCopy, $scope.POIs, poiEqualityComparator, keyFunction);
 
 		$ctrl.submit =function submit(diff, rsaPrivateKey) {
 			dataService.sendMaliciousChanges(diff, rsaPrivateKey).then(function () {
@@ -22,6 +24,64 @@ var def = {
 				dataService.savePublicKey('service-provider', $scope.keypair.public);
 			}
 		};
+
+		$ctrl.voronoiChangesToggled = function voronoiChangesToggled(changeVoronoi) {
+			var existing = $scope.POIsCopy;
+			var pois = $scope.POIs;
+			if (changeVoronoi && pois[0]) {
+				if (pois[0]._meta_.neighborsServiceProvider) {
+					for (var i = 0; i < pois.length; i++) {
+						pois[i]._meta_.neighbors = pois[i]._meta_.neighborsServiceProvider;
+					}
+				} else {
+					var vordiag = voronoiDiagram(pois);
+					for (var i = 0; i < (vordiag && vordiag.cells && vordiag.cells.length || 0); i++) {
+						var cell = vordiag.cells[i];
+						var neighborCells = cell.getNeighbors();
+						if (!cell.owner._meta_.neighborsDataowner)
+							cell.owner._meta_.neighborsDataowner = cell.owner._meta_.neighbors;
+						cell.owner._meta_.neighbors = neighborCells.map(function (cell) {
+							if (!cell.owner.location) {
+								cell.owner.location = {
+									'lat': cell.owner.lat,
+									'lng': cell.owner.lng
+								};
+							}
+							return cell.owner;
+						});
+						cell.owner.neighbors = cell.owner._meta_.neighbors;
+						cell.owner._meta_.neighborsServiceProvider = cell.owner._meta_.neighbors;
+					}
+
+				}
+			} else if (!changeVoronoi && pois[0]) {
+				if (pois[0]._meta_.neighborsDataowner) {
+					for (var i = 0; i < pois.length; i++) {
+						pois[i]._meta_.neighbors = pois[i]._meta_.neighborsDataowner;
+					}
+				}
+			}
+
+			$scope.diff = util.diffArrays(existing, pois, poiEqualityComparator, keyFunction);
+		};
+
+		function poiEqualityComparator(old, latest, keyFunc) {
+			if (old.lat == latest.lat && old.lng == latest.lng
+				&& old.tail.address == latest.tail.address && old.tail.name == latest.tail.name && old.tail.phone == latest.tail.phone) {
+					if ((old._meta_.neighbors || []).length != (latest._meta_.neighbors || []).length || old._meta_.verificationObject != latest._meta_.verificationObject)
+						return false;
+					for (var i = 0; i < (old._meta_.neighbors || []).length; i++) {
+						if (old._meta_.neighbors[i].location.lat != latest._meta_.neighbors[i].location.lat || old._meta_.neighbors[i].location.lng != latest._meta_.neighbors[i].location.lng)
+							return false;
+					}
+					return true;
+			}
+			return false;
+		}
+
+		function keyFunction(point) {
+			return point.lat.toString() + "," + point.lng.toString();
+		}
 	}],
 	'getName': function () {
 		return CONTROLLER_NAME;
--- frontend/src/ngapp/views/service-provider/service-provider-component.js
+++ frontend/src/ngapp/views/service-provider/service-provider-component.js
@@ -1,7 +1,6 @@
 import ng from 'angular';
 import ngapp from '../../ngappmodule.js';
 import dataService from '../../services/data.js';
-import voronoiDiagram from '../../../voronoi-util/voronoi.js';
 import util from '../../services/util.js';
 import maliciousSignatureModalController from './maliciousSignatureModalController.js';
 
@@ -14,7 +13,7 @@ var def = {
 		$q.all([dataService.getPoints()]).then(function (resolves) {
 			var points = resolves[0];
 			$scope['POIs'] = ($scope['POIs'] || []).concat(points);
-			$ctrl.POIsCopy = ng.copy($scope['POIs']);
+			$scope.POIsCopy = ng.copy($scope['POIs']);
 		});
 
 		$ctrl.addPoint = function () {
@@ -22,25 +21,6 @@ var def = {
 		};
 
 		$ctrl.submitChanges = function (pois) {
-			var existing = $ctrl.POIsCopy;
-			var vordiag = voronoiDiagram(pois);
-			for (var i = 0; i < (vordiag && vordiag.cells && vordiag.cells.length || 0); i++) {
-				var cell = vordiag.cells[i];
-				var neighborCells = cell.getNeighbors();
-				cell.owner._meta_.neighbors = neighborCells.map(function (cell) {
-					if (!cell.owner.location) {
-						cell.owner.location = {
-							'lat': cell.owner.lat,
-							'lng': cell.owner.lng
-						};
-					}
-					return cell.owner;
-				});
-				cell.owner.neighbors = cell.owner._meta_.neighbors;
-			}
-			var diff = util.diffArrays(existing, pois, poiEqualityComparator, keyFunction);
-			$scope.diff = diff;
-
 			ngDialog.open({
 				template: require('!raw-loader!./malicious-signature-modal.html'),
 				plain: true,
@@ -51,24 +31,6 @@ var def = {
 				controllerAs: '$ctrl'
 			});
 		};
-
-		function poiEqualityComparator(old, latest, keyFunc) {
-			if (old.lat == latest.lat && old.lng == latest.lng
-				&& old.tail.address == latest.tail.address && old.tail.name == latest.tail.name && old.tail.phone == latest.tail.phone) {
-					if ((old._meta_.neighbors || []).length != (latest._meta_.neighbors || []).length || old._meta_.verificationObject != latest._meta_.verificationObject)
-						return false;
-					for (var i = 0; i < (old._meta_.neighbors || []).length; i++) {
-						if (old._meta_.neighbors[i].location.lat != latest._meta_.neighbors[i].location.lat || old._meta_.neighbors[i].location.lng != latest._meta_.neighbors[i].location.lng)
-							return false;
-					}
-					return true;
-			}
-			return false;
-		}
-
-		function keyFunction(point) {
-			return point.lat.toString() + "," + point.lng.toString();
-		}
 	}],
 	controllerName: COMPONENT_NAME + 'Controller',
 	componentDdo: {
