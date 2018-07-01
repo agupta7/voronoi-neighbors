9686e1682dd5e6028dce01948f48d2f41053c678
Anuj Gupta <azg0076@auburn.edu>
2018-07-01 00:17:01 -0500

    No longer importing scripts from cloudfront.

--- frontend/test/webpack-testBuild.js
+++ frontend/test/webpack-testBuild.js
@@ -1,3 +1,6 @@
+var jasmineRequire = require('jasmine-core/lib/jasmine-core/jasmine.js');
+require('imports-loader?jasmineRequire=jasmine-core/lib/jasmine-core/jasmine.js!jasmine-core/lib/jasmine-core/jasmine-html.js');
+require('imports-loader?jasmineRequire=jasmine-core/lib/jasmine-core/jasmine.js!jasmine-core/lib/jasmine-core/boot.js');
 import ngapp from '../src/webpack-entry.js';
 require('angular-mocks/ngMock');
 
